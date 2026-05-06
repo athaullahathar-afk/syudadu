3f8e8f0932a4300f709cf278fa61879e63e7ca45
kelompokbelajar <athaullah.athar@gmail.com>
2026-05-06 20:14:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 class ConfessionSite {
     constructor() {
         this.dates = [
-            { day: 'Thu, 10 May', time: '9:00 PM', full: 'Sunday, May 10th at 9:00 PM' },
-            { day: 'Sat, 13 May', time: '9:00 PM', full: 'Friday, May 13th at 9:00 PM' },
-            { day: 'Mon, 14 May', time: '9:00 PM', full: 'Saturday, MAy 14th at 9:00 PM' },
-            { day: 'Wed, 15 May', time: '9:00 PM', full: 'Sunday, May 15th at 9:00 PM' },
-            { day: 'Wed, 16 May', time: '9:00 PM', full: 'Sunday, May 16th at 9:00 PM' }
+            { day: 'Thu, 10 May', time: '10:00 WITA', full: 'Sunday, May 10th at 10:00 WITA' },
+            { day: 'Wed, 13 May', time: '10:00 WITA', full: 'Wednesday, May 13th at 9:00 WITA' },
+            { day: 'Mon, 14 May', time: '10:00 WITA', full: 'Thursday, MAy 14th at 9:00 WITA' },
+            { day: 'Wed, 15 May', time: '10:00 WITA', full: 'Friday, May 15th at 9:00 WITA' },
+            { day: 'Wed, 16 May', time: '10:00 WITA', full: 'Saturday, May 16th at 9:00 WITA' }
         ];
         this.selectedDate = null;
         this.waNumber = '+6285701037489'; 
@@ -51,7 +51,7 @@ class ConfessionSite {
     redirectToWhatsApp() {
         if (!this.selectedDate) return;
         
-        const message = `Halo ganteng! wkwkwk I picked ${this.selectedDate.full} for our meetup 😊 Can we do Google Meet then?`;
+        const message = `Halo ganteng! wkwkwk Aku pilih tanggal ${this.selectedDate.full} for our meetup yaa 😊`;
         const encodedMessage = encodeURIComponent(message);
         const waUrl = `https://wa.me/${this.waNumber}?text=${encodedMessage}`;
         
